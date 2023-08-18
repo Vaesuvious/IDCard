fd890c8ea9109b01001786dc13c30f685d91d183
A.Chambers <57132456+Vaesuvious@users.noreply.github.com>
2023-08-18 17:51:03 -0400

    deploy IDCard

---
+++ IdCard.js
@@ -0,0 +1,28 @@
+function idCard(){
+    var firstName = document.getElementById("userFirstName").value;
+    var lastName = document.getElementById("userLastName").value;
+    var address = document.getElementById("userAddress").value;
+    var age = parseInt(document.getElementById('userAge').value);
+    var phoneNumber = parseInt(document.getElementById('userPhoneNum').value);
+    
+    document.getElementById("postFullName").innerHTML = firstName + " " + lastName;
+    document.getElementById("postAddress").innerHTML = "Addres:" + address;
+    
+    var numberArray = [];
+    numberArray.push(age);
+    numberArray.push(phoneNumber);
+    
+    for(var i= 0; i < numberArray.length; i++){
+      if(numberArray[i] <= 100){
+        document.getElementById("postAge").innerHTML = "Age: " + age;
+      }
+      else if(numberArray[i] > 100){
+        document.getElementById("postPhoneNumber").innerHTML = "Phone: " + phoneNumber;
+      }
+      else{
+        alert("Invalid age or phone number!");
+      }
+      
+      
+    }
+  }
